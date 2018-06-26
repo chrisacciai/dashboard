d4e84e57bb8a23770e25572ed22b5b4b8d4879e7
Christopher <chris@acciai.us>
2018-06-26 15:06:15 -0500

    radialbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import {BarChart, LineChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import {BarChart, RadialBarChart, RadialBar, LineChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 import './App.css';
 
 const data = [
@@ -12,6 +12,22 @@ const data = [
   {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
   {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
 ];
+
+const data2 = [
+  {name: '18-24', uv: 31.47, pv: 2400, fill: '#8884d8'},
+  {name: '25-29', uv: 26.69, pv: 4567, fill: '#83a6ed'},
+  {name: '30-34', uv: 15.69, pv: 1398, fill: '#8dd1e1'},
+  {name: '35-39', uv: 8.22, pv: 9800, fill: '#82ca9d'},
+  {name: '40-49', uv: 8.63, pv: 3908, fill: '#a4de6c'},
+  {name: '50+', uv: 2.63, pv: 4800, fill: '#d0ed57'},
+  {name: 'unknow', uv: 6.67, pv: 4800, fill: '#ffc658'}
+];
+
+const style = {
+  top: 0,
+  left: 350,
+  lineHeight: '24px'
+};
 
 class App extends Component {
   render() {
@@ -33,6 +49,11 @@ class App extends Component {
           <Bar dataKey="pv" fill="#8884d8" />
           <Bar dataKey="uv" fill="#82ca9d" />
           </BarChart>
+
+          <RadialBarChart width={500} height={300} cx={150} cy={150} innerRadius={20} outerRadius={140} barSize={10} data={data2}>
+          <RadialBar minAngle={15} label={{ position: 'insideStart', fill: '#fff' }} background clockWise={true} dataKey='uv'/>
+          <Legend iconSize={10} width={120} height={140} layout='vertical' verticalAlign='middle' wrapperStyle={style}/>
+          </RadialBarChart>
         </div>
         <div class = "column">
           <LineChart width={600} height={300} data={data}
