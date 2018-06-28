7b96bd8eb42822a100e5188c18de10cbcea8e633
Christopher <chris@acciai.us>
2018-06-28 14:16:26 -0500

    charts edit

--- src/Chart1.js
+++ src/Chart1.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
 import {Button} from 'react-bootstrap';
 import {ButtonGroup} from 'react-bootstrap';
 
 const data = [
-    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-    {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-    {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-    {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+    {month: 'Jan-18', pv: 74},
+    {month: 'Feb-18', pv: 87},
+    {month: 'Mar-18', pv: 92},
+    {month: 'Apr-18', pv: 112},
+    {month: 'May-18', pv: 160},
+    {month: 'MTD 6/18/18', pv: 136},
   ];
 
 export default class Chart1 extends Component {
@@ -39,16 +38,14 @@ export default class Chart1 extends Component {
         return ( 
             <div>
             <p style={ shown }>
-            <BarChart width={600} height={300} data={data}
+            <LineChart width={600} height={300} data={data}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-            <XAxis dataKey="name"/>
+            <XAxis dataKey="month"/>
             <YAxis/>
             <CartesianGrid strokeDasharray="3 3"/>
             <Tooltip/>
-            <Legend />
-            <Bar dataKey="pv" fill="#8884d8" />
-            <Bar dataKey="uv" fill="#82ca9d" />
-            </BarChart>
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+            </LineChart>
             </p>
             <p style={ hidden }>Edit Data Here...</p>
             <ButtonGroup>
--- src/Chart2.js
+++ src/Chart2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import {BarChart, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
 import {Button} from 'react-bootstrap';
 import {ButtonGroup} from 'react-bootstrap';
 
@@ -39,16 +39,13 @@ export default class Chart2 extends Component {
         return ( 
             <div>
             <p style={ shown }>
-            <LineChart width={600} height={300} data={data}
+            <BarChart width={600} height={300} data={data}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
             <XAxis dataKey="name"/>
             <YAxis/>
             <CartesianGrid strokeDasharray="3 3"/>
             <Tooltip/>
-            <Legend />
-            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
+            </BarChart>
             </p>
             <p style={ hidden }>Edit Data Here...</p>
             <ButtonGroup>
