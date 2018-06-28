e1decacdad0d1d01dd7e932cfe905e4287883754
Christopher <chris@acciai.us>
2018-06-27 19:10:16 -0500

    data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import {BarChart, Radar, Brush, ReferenceLine, RadarChart, PolarGrid, PolarAngle
 import './App.css';
 import {Chart1} from './Chart1.js';
 
+
+const data7 = [
+  {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+  {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+  {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+  {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+  {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+  {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+  {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
+
 const data2 = [
   {name: '18-24', uv: 31.47, pv: 2400, fill: '#8884d8'},
   {name: '25-29', uv: 26.69, pv: 4567, fill: '#83a6ed'},
@@ -105,7 +116,7 @@ class App extends Component {
         </div>
         <div class = "column">
           <br/>
-          <LineChart width={600} height={300} data={data}
+          <LineChart width={600} height={300} data={data7}
           margin={{top: 5, right: 30, left: 20, bottom: 5}}>
           <XAxis dataKey="name"/>
           <YAxis/>
