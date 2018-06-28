8d422915879352db9d757ec936c2f6cea8620901
Christopher <chris@acciai.us>
2018-06-27 19:19:15 -0500

    chart2

--- src/App.js
+++ src/App.js
@@ -3,17 +3,7 @@ import logo from './logo.svg';
 import {BarChart, Radar, Brush, ReferenceLine, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, RadialBarChart, RadialBar, LineChart, AreaChart, Area, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 import './App.css';
 import Chart1 from './Chart1.js';
-
-
-const data7 = [
-  {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-  {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-  {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-  {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-  {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-  {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-  {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
+import Chart2 from './Chart2.js';
 
 const data2 = [
   {name: '18-24', uv: 31.47, pv: 2400, fill: '#8884d8'},
@@ -116,16 +106,7 @@ class App extends Component {
         </div>
         <div class = "column">
           <br/>
-          <LineChart width={600} height={300} data={data7}
-          margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-          <XAxis dataKey="name"/>
-          <YAxis/>
-          <CartesianGrid strokeDasharray="3 3"/>
-          <Tooltip/>
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-          </LineChart>
+          <Chart2/>
 
           <RadarChart cx={300} cy={250} outerRadius={150} width={600} height={500} data={data3}>
           <PolarGrid />
