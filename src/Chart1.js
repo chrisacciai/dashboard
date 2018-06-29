4d3ff6fca1fc77ee317675b158d6f5c3c6e8f322
Christopher <chris@acciai.us>
2018-06-29 09:37:02 -0500

    responsive

--- src/Chart1.js
+++ src/Chart1.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ReferenceLine} from 'recharts';
+import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ReferenceLine} from 'recharts';
 import {Button} from 'react-bootstrap';
 import {ButtonGroup} from 'react-bootstrap';
 
@@ -49,15 +49,18 @@ export default class Chart1 extends Component {
             </div>
             <br/>
             <p style={ shown }>
-              <LineChart width={600} height={300} data={data}
+            <ResponsiveContainer>
+              <LineChart data={data}
               margin={{top: 0, right: 30, left: 15, bottom: 5}}>
               <XAxis dataKey="month"/>
               <YAxis/>
               <CartesianGrid strokeDasharray="3 3"/>
               <ReferenceLine y={96} stroke="blue" strokeDasharray="3 3" />
-              <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>              </LineChart>
-              </p>
-              <p style={ hidden }>Edit Data Here...</p>
+              <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+              </LineChart>
+            </ResponsiveContainer>
+            </p>
+            <p style={ hidden }>Edit Data Here...</p>
           </div>
         );
     }
