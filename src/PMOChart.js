b3143480b9afc372a51e9fdf2c0f5431e8033e8c
Christopher <chris@acciai.us>
2018-07-12 15:39:21 -0500

    pie

--- src/PMOChart.js
+++ src/PMOChart.js
@@ -63,8 +63,9 @@ export default class PMOChart extends Component {
             <br/>
             <p style={ shown }>
             <div id="container">
-    	        <PieChart>
-                <Pie data={this.state.chartData} fill="#8884d8" label/>
+              <PieChart width={800} height={400}>
+                <Pie isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill="#8884d8" label/>
+                <Pie data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
               </PieChart>
             </div>
             </p>
