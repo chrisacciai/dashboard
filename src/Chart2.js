21ee58fb157502c56c550c90a2d5277183515c14
Christopher <chris@acciai.us>
2018-07-16 17:10:54 -0500

    huh

--- src/Chart2.js
+++ src/Chart2.js
@@ -147,10 +147,10 @@ export default class Chart2 extends Component {
                 <LineChart data={this.state.items}
                 margin={{top: 0, right: 30, left: 15, bottom: 5}}>
                 <XAxis dataKey='month'/>
-                <YAxis formatter={toPercent}/>
+                <YAxis/>
                 <CartesianGrid strokeDasharray="3 3"/>
                 <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}>
-                  <LabelList dataKey='pv' position='top' formatter={toPercent}/>
+                  <LabelList dataKey="pv" position='bottom'/>
                 </Line>
                 </LineChart>
               </ResponsiveContainer>
