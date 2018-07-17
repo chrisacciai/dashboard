7200dc8e8f151025679b2b8e3206d81e3810eb7d
Christopher <chris@acciai.us>
2018-07-17 14:53:51 -0500

    small change

--- src/Chart5.js
+++ src/Chart5.js
@@ -201,8 +201,8 @@ export default class Chart5 extends Component {
             <div id="container">
               <ResponsiveContainer width="100%" height="100%">
                 <LineChart data ={this.state.items}
-                margin={{top: 0, right: 35, left: 15, bottom: 20}}>
-                <XAxis dataKey='month' tick={{angle: -45}} tickMargin='11' interval={0}/>
+                margin={{top: 0, right: 35, left: 15, bottom: 24}}>
+                <XAxis dataKey='month' tick={{angle: -45}} tickMargin='12' interval={0}/>
                 <YAxis />
                 <CartesianGrid strokeDasharray="3 3"/>
                 <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}>
