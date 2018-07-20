3486d7224bec1a75c4bbc31cdfe40fc03948941e
Christopher <chris@acciai.us>
2018-07-20 13:51:35 -0500

    chart1

---
+++ src/BD_Chart1.js
@@ -0,0 +1,173 @@
+import React, { Component } from 'react';
+import {ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, LabelList} from 'recharts';
+import {Table, FormControl, ButtonGroup, Button} from 'react-bootstrap';
+import firebase from './firebase.js';
+
+const toPercent = (decimal, fixed = 0) => {
+  return `${(decimal * 100).toFixed(fixed)}%`;
+}
+
+export default class BD_Chart1 extends Component {
+    constructor() {
+        super();
+        this.state = {
+          shown: true,
+          lineOneMonth: null,
+          lineOneData: null,
+          lineTwoMonth: null,
+          lineTwoData: null,
+          lineThreeMonth: null,
+          lineThreeData: null,
+          items: null
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }	
+      
+      hide() {
+        this.setState({
+          shown: false
+        });
+      }
+      show() {
+        this.setState({
+          shown: true
+        })
+      }
+
+      handleChange(event) {
+        this.setState({ [event.target.name] : event.target.value });
+      }
+
+      handleSubmit(e) {
+        e.preventDefault();
+        const dataRef = firebase.database().ref('BDChartOneData');
+        const monthDataPair = {
+          month1: this.state.lineOneMonth,
+          value1: parseFloat(this.state.lineOneData),
+          value1: parseFloat(this.state.lineOneData),
+          month2: this.state.lineTwoMonth,
+          value2: parseFloat(this.state.lineTwoData),
+          value2: parseFloat(this.state.lineTwoData),
+          month3: this.state.lineThreeMonth,
+          value3: parseFloat(this.state.lineThreeData),
+          value3: parseFloat(this.state.lineThreeData),
+          
+        }
+        dataRef.set(monthDataPair);
+      }
+
+      componentDidMount() {
+        const dataRef = firebase.database().ref('BDChartOneData');
+        dataRef.on('value', (snapshot) => {
+          let items = snapshot.val();
+          let newState = [];
+          newState.push({
+            month: items.month1,
+            pv: items.value1,
+          });
+          newState.push({
+            month: items.month2,
+            pv: items.value2,
+          });
+          newState.push({
+            month: items.month3,
+            pv: items.value3,
+          });
+
+          this.setState({
+            items: newState,
+            lineOneMonth: items.month1,
+            lineOneData: items.value1,
+            lineTwoMonth: items.month2,
+            lineTwoData: items.value2,
+            lineThreeMonth: items.month3,
+            lineThreeData: items.value3,
+          });
+        });
+      }
+
+      render() {
+        
+        var shown = {
+          display: this.state.shown ? "block" : "none"
+        };
+        
+        var hidden = {
+          display: this.state.shown ? "none" : "block"
+        }
+
+        return (
+          <div>
+            <br/>
+            <div>
+              <p class="alignleft">Growth</p>
+              <p class="alignright">
+                <ButtonGroup bsSize="xs">
+                  <Button onClick={this.show.bind(this)}>Chart View</Button>
+                  <Button onClick={this.hide.bind(this)}>Edit Data</Button>
+                  <Button type="submit" bsStyle="primary" form="form7">Submit Data</Button> 
+                </ButtonGroup>
+              </p>
+            </div>
+            <br/>
+            <p style={ shown }>
+            <div id="container">
+              <ResponsiveContainer width="100%" height="100%">
+                <BarChart data ={this.state.items}
+                margin={{top: 0, right: 50, left: 15, bottom: 5}}>
+                <XAxis dataKey='month'/>
+                <YAxis tickFormatter={toPercent}/>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <Bar dataKey="pv" fill="#8884d8">
+                  <LabelList dataKey='pv' position='top' formatter={toPercent} />
+                </Bar>
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+            </p>
+            <p style={ hidden }>
+              <div id="table4">
+                <form id="form7" onSubmit={this.handleSubmit}>
+                <Table striped bordered condensed hover>
+                <thead>
+                  <tr>
+                    <th>Month</th>
+                    <th>Rate</th>
+                  </tr>
+                </thead>
+                  <tbody>
+                    <tr>
+                        <td>
+                              <FormControl type="text" name="lineOneMonth" onChange={this.handleChange} value={this.state.lineOneMonth} />
+                        </td>
+                        <td>
+                              <FormControl type="text"name="lineOneData" onChange={this.handleChange} value={this.state.lineOneData} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                              <FormControl type="text" name="lineTwoMonth" onChange={this.handleChange} value={this.state.lineTwoMonth} />
+                        </td>
+                        <td>
+                              <FormControl type="text" name="lineTwoData" onChange={this.handleChange} value={this.state.lineTwoData} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                              <FormControl type="text" name="lineThreeMonth" onChange={this.handleChange} value={this.state.lineThreeMonth} />
+                        </td>
+                        <td>
+                              <FormControl type="text" name="lineThreeData" onChange={this.handleChange} value={this.state.lineThreeData} />
+                        </td>
+                    </tr>
+                  </tbody>
+                </Table>
+                </form>
+                </div>
+              </p>
+          </div>
+        );
+    }
+}
