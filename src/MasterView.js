fa2d4f90f287b687f766ddbabd709f9255106754
Christopher <chris@acciai.us>
2018-07-20 17:59:29 -0500

    master

--- src/MasterView.js
+++ src/MasterView.js
@@ -6,15 +6,22 @@ import M_Chart3 from './OpsCharts/M_Chart3.js';
 export default class MasterView extends Component {
       render() {
         return (
-            <div class = "row">
-                <div class = "columnThirds">
-                    <M_Chart1/>
+            <div>
+                <div class = "MasterHeader">
+                    <p>
+                        Operations
+                    </p>
                 </div>
-                <div class = "columnThirds">
-                    <M_Chart2/>
-                </div>
-                <div class = "columnThirds">
-                    <M_Chart3/>
+                <div class = "row">
+                    <div class = "columnThirds">
+                        <M_Chart1/>
+                    </div>
+                    <div class = "columnThirds">
+                        <M_Chart2/>
+                    </div>
+                    <div class = "columnThirds">
+                        <M_Chart3/>
+                    </div>
                 </div>
             </div>
         );
