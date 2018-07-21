b2823427d658c255d1400aab84fd68b5f560cfea
Christopher <chris@acciai.us>
2018-07-20 19:04:14 -0500

    test

--- src/MasterView.js
+++ src/MasterView.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import M_Chart1 from './OpsCharts/M_Chart1.js';
 import M_Chart2 from './OpsCharts/M_Chart2.js';
 import M_Chart3 from './OpsCharts/M_Chart3.js';
+import ReactToPrint from 'react-to-print';
 
 export default class MasterView extends Component {
       render() {
@@ -27,3 +28,17 @@ export default class MasterView extends Component {
         );
     }
 }
+
+class Example extends React.Component {
+    render() {
+      return (
+        <div>
+          <ReactToPrint
+            trigger={() => <a href="#">Print this out!</a>}
+            content={() => this.componentRef}
+          />
+          <ComponentToPrint ref={el => (this.componentRef = el)} />
+        </div>
+      );
+    }
+  }
