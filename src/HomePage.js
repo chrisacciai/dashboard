5a52b8b3892b13f569b81b8b50379642bdd15073
Christopher <chris@acciai.us>
2018-07-23 15:15:20 -0500

    again

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ControlledTabs from './ControlledTabs';
-import logo from './logo';
 
 const HomePage = () =>
     <div className="App">
         <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">[BETA] Dash</h1>
         </header>
         <ControlledTabs/>
