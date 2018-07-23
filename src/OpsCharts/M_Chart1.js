bf256c81d8a940a2a362eaee7fa93bcf930ac8c5
Christopher <chris@acciai.us>
2018-07-23 13:56:16 -0500

    font size

--- src/OpsCharts/M_Chart1.js
+++ src/OpsCharts/M_Chart1.js
@@ -56,8 +56,8 @@ export default class M_Chart1 extends Component {
             <div>
               <LineChart data={this.state.items} width={370} height={200}
                 margin={{top: 10, right: 17, left: 0, bottom: 10}}>
-                <XAxis dataKey='month'/>
-                <YAxis/>
+                <XAxis dataKey='month' tick={{fontSize= 10}}/>
+                <YAxis tick={{fontSize= 10}}/>
                 <CartesianGrid strokeDasharray="3 3"/>
                 <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}>
                   <LabelList dataKey='pv' position='bottom' />
--- src/OpsCharts/M_Chart2.js
+++ src/OpsCharts/M_Chart2.js
@@ -59,8 +59,8 @@ componentDidMount() {
         <div>
           <LineChart data={this.state.items} width={370} height={200}
             margin={{top: 10, right: 17, left: 0, bottom: 10}}>
-            <XAxis dataKey='month'/>
-            <YAxis tickFormatter={toPercent}/>
+            <XAxis dataKey='month' tick={{fontSize= 10}}/>
+            <YAxis tickFormatter={toPercent} tick={{fontSize= 10}}/>
             <CartesianGrid strokeDasharray="3 3"/>
             <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}>
               <LabelList dataKey="pv" position='bottom' formatter={toPercent}/>
