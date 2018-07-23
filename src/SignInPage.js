76919cb34267236be74294735978734b28cd60ee
Christopher <chris@acciai.us>
2018-07-23 16:22:44 -0500

    imports

--- src/SignInPage.js
+++ src/SignInPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
-import { auth } from '../firebase';
+import { auth } from './firebase/firebase';
 import * as routes from '../constants/routes';
 
 const SignInPage = ({ history }) =>
