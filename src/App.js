e889c485a56058c88119af7c06b196fd52635d3d
Christopher <chris@acciai.us>
2018-07-23 15:25:45 -0500

    app css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Navigation from './Navigation';
-import 
 
 import LandingPage from './LandingPage';
 import SignInPage from './SignInPage';
