7bc8d76f43a19a85f31d461a8d4a2c86db3e7179
Christopher <chris@acciai.us>
2018-07-23 16:51:09 -0500

    accounts gone

--- src/Navigation.js
+++ src/Navigation.js
@@ -16,7 +16,6 @@ const NavigationAuth = () =>
   <ul>
     <li><Link to={routes.LANDING}>Landing</Link></li>
     <li><Link to={routes.HOME}>Home</Link></li>
-    <li><Link to={routes.ACCOUNT}>Account</Link></li>
     <li><SignOutButton /></li>
   </ul>
 
