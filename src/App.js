c340bac1675c0e186d7b29559ba0a517ce1eb329
Christopher <chris@acciai.us>
2018-07-24 12:38:48 -0500

    button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Button} from 'react-bootstrap';
 import ControlledTabs from './ControlledTabs';
 
 class App extends Component {
@@ -10,7 +9,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">[BETA] Dash</h1>
-          <Button className="Button1" bsStyle="primary">Export as PDF</Button>
         </header>
         <ControlledTabs/>
       </div>
