9bf6c795eb9be6bad1e45ca6463827a8dc86c38a
Christopher <chris@acciai.us>
2018-07-25 20:33:32 -0500

    test

--- src/Export.js
+++ src/Export.js
@@ -4,42 +4,12 @@ import jsPDF from 'jspdf';
 import MChart1 from './OpsCharts/MChart1.js';
 import MChart2 from './OpsCharts/MChart2.js';
 import MChart3 from './OpsCharts/MChart3.js';
-import MChart4 from './OpsCharts/MChart4.js';
-import MChart5 from './OpsCharts/MChart5.js';
-import MChart6 from './OpsCharts/MChart6.js';
-import {Button, FormControl} from 'react-bootstrap';
-import logo from './logo-dark.png'
-import firebase from './Firebase.js';
+import {Button} from 'react-bootstrap';
 
 export default class Export extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      week: null,
-    };
   }
-
-  handleChange(event) {
-    this.setState({ [event.target.name] : event.target.value });
-    event.preventDefault();
-    const dataRef = firebase.database().ref('weekData');
-    const monthDataPair = {
-      week: this.state.week, 
-    }
-    dataRef.set(monthDataPair);
-  }
-
-  componentDidMount() {
-    const dataRef = firebase.database().ref('weekData');
-    dataRef.on('value', (snapshot) => {
-      let items = snapshot.val();
-
-      this.setState({
-        week: items.week,
-      });
-    });
-  }
-
 
   printDocument() {
     const input = document.getElementById('divToPrint');
@@ -56,12 +26,10 @@ export default class Export extends Component {
   }
 
   render() {
-    return (
-    <div>
-      <Button className="Button1" onClick={this.printDocument} bsStyle="primary">Export as PDF</Button>
-      <FormControl bsStyle="small" className="week-button" type="text" name="week" onChange={this.handleChange} value={this.state.week}/>
-        <div id="divToPrint" className="mt4">
-          <div class = "MasterTitle">
+    return (<div> <Button className="Button1" onClick={this.printDocument} bsStyle="primary">Export as PDF</Button>
+      <div id="divToPrint" className="mt4">
+        <div>
+          <div class = "MasterHeader">
             <p className = "MasterText">
               Operations
             </p>
@@ -69,18 +37,16 @@ export default class Export extends Component {
           <div class = "rowThirds">
             <div class = "columnThirds">
               <MChart1/>
-              <MChart4/>
             </div>
             <div class = "columnThirds">
               <MChart2/>
-              <MChart5/>
             </div>
             <div class = "columnThirds">
               <MChart3/>
-              <MChart6/>
             </div>
           </div>
         </div>
+      </div>
     </div>);
   }
 }
