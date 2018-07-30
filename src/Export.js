580992c96750a254073513f9e3d9c6b5e76c1262
Christopher <chris@acciai.us>
2018-07-30 15:27:29 -0500

    test

--- src/Export.js
+++ src/Export.js
@@ -71,16 +71,62 @@ export default class Export extends Component {
         const pdf = new jsPDF({
           orientation: 'landscape'
         });
-        pdf.addImage(imgData, 'JPEG', 0, 0);
-        pdf.addPage()
-        const page2 = document.getElementById('divToPrint2');
-        html2canvas(page2)
-        .then((canvas) => {
-          const imgData = canvas.toDataURL('image/png');
-          pdf.addImage(imgData, 'JPEG', 0, 0);   
-        });      
-      })
+        pdf.addImage(imgData, 'JPEG',0, 0, {pagesplit: true});  
+        });
     ;
+  }
+
+  makePDF() {
+
+    var quotes = document.getElementById('testDiv');
+
+    html2canvas(quotes, {
+        onrendered: function(canvas) {
+
+        //! MAKE YOUR PDF
+        var pdf = new jsPDF('p', 'pt', 'letter');
+
+        for (var i = 0; i <= quotes.clientHeight/980; i++) {
+            //! This is all just html2canvas stuff
+            var srcImg  = canvas;
+            var sX      = 0;
+            var sY      = 980*i; // start 980 pixels down for every new page
+            var sWidth  = 900;
+            var sHeight = 980;
+            var dX      = 0;
+            var dY      = 0;
+            var dWidth  = 900;
+            var dHeight = 980;
+
+            window.onePageCanvas = document.createElement("canvas");
+            onePageCanvas.setAttribute('width', 900);
+            onePageCanvas.setAttribute('height', 980);
+            var ctx = onePageCanvas.getContext('2d');
+            // details on this usage of this function: 
+            // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images#Slicing
+            ctx.drawImage(srcImg,sX,sY,sWidth,sHeight,dX,dY,dWidth,dHeight);
+
+            // document.body.appendChild(canvas);
+            var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
+
+            var width         = onePageCanvas.width;
+            var height        = onePageCanvas.clientHeight;
+
+            //! If we're on anything other than the first page,
+            // add another page
+            if (i > 0) {
+                pdf.addPage(612, 791); //8.5" x 11" in pts (in*72)
+            }
+            //! now we declare that we're working on that page
+            pdf.setPage(i+1);
+            //! now we add content to that page!
+            pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width*.62), (height*.62));
+
+        }
+        //! after the for loop is finished running, we save the pdf.
+        pdf.save('Test.pdf');
+      }
+    });
   }
 
   render() {
@@ -88,12 +134,13 @@ export default class Export extends Component {
     <div>
       <ButtonGroup className="Button1">
         <Button type="submit" bsStyle="primary" form="weekForm">Set Date</Button>
-        <Button onClick={this.printDocument} bsStyle="primary">Export as PDF</Button>
+        <Button onClick={this.makePDF} bsStyle="primary">Export as PDF</Button>
       </ButtonGroup>
       <form id="weekForm" onSubmit={this.handleSubmit}>
         <FormControl bsStyle="small" className="week-button" type="text" name="week" onChange={this.handleChange} value={this.state.week}/>
       </form>
-      <div id="divToPrint" className="mt4">
+      <div id="testDiv">
+        <div id="divToPrint" className="mt4">
           <div id="master-header">
             <div class= "logo-master">
               <img src={logo} alt="logo"/>
@@ -122,7 +169,7 @@ export default class Export extends Component {
           </div>
         </div>
         <br/>
-        <div id="divToPrint2" className="mt4">
+        <div id="divToPrint" className="mt4">
           <div class = "MasterTitle">
             <p className = "MasterText">
               Finance
@@ -156,7 +203,7 @@ export default class Export extends Component {
           </div>
         </div>
         <br/>
-        <div id="divToPrint3" className="mt4">
+        <div id="divToPrint" className="mt4">
           <div class = "MasterTitle">
             <p className = "MasterText">
               Business Development
@@ -198,6 +245,7 @@ export default class Export extends Component {
             </div>
           </div>
         </div>
+      </div>
     </div>);
   }
 }
